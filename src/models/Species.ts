7bf19e6c88d5d445883579b282a93b7157756d6e
rodrigorparedes <rodrigorparedes@gmail.com>
2024-10-08 23:12:58 -0400

    add color, description and imageUrl to species

--- src/models/Species.ts
+++ src/models/Species.ts
@@ -1,5 +1,8 @@
 interface Species {
     commonName: string,
     scientificName: string,
-    foliage: string
+    foliage: string,
+    imageUrl: string,
+    color: string,
+    description: string
 }
